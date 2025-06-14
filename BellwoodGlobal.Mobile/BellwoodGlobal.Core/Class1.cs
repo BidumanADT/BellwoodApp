6f91a03d256774257eeee9a178f208ed273d1e59
sgtadt <sgtadt@gmail.com>
2025-06-14 18:07:49 -0500

    chore: initial solution scaffold (mobile + API + core)

---
+++ BellwoodGlobal.Mobile/BellwoodGlobal.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BellwoodGlobal.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BellwoodGlobal.Mobile/BellwoodGlobal.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BellwoodGlobal.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
