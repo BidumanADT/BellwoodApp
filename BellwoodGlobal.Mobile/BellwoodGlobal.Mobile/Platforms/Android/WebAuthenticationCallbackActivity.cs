8849653eb43c053a81099d6f84006cbbef427ed6
sgtadt <sgtadt@gmail.com>
2025-07-27 19:02:11 -0500

    finding more data scheme mis-namings --fixed

--- BellwoodGlobal.Mobile/BellwoodGlobal.Mobile/Platforms/Android/WebAuthenticationCallbackActivity.cs
+++ BellwoodGlobal.Mobile/BellwoodGlobal.Mobile/Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -8,13 +8,12 @@ namespace BellwoodGlobal.Mobile
     [Activity(NoHistory = true,
               Exported = true,
               LaunchMode = LaunchMode.SingleTop)]
-    [IntentFilter(new[] { Intent.ActionView },
-                  Categories = new[] {
-                    Intent.CategoryDefault,
-                    Intent.CategoryBrowsable
-                  },
-                  DataScheme = "com.bellwoodglobal.mobile",
-                  DataHost = "callback")]
+    [IntentFilter(
+      new[] { Intent.ActionView },
+      Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+      DataScheme = "com.bellwoodglobal.mobile",
+      DataHost = "callback"
+    )]
     public class WebAuthenticationCallbackActivity : WebAuthenticatorCallbackActivity
     {
         // no overrides needed here
