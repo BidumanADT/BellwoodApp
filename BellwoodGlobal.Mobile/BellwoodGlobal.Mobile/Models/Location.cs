1cc3d69a4c5773b71ac949b22b7b314a48e98ab4
sgtadt <sgtadt@gmail.com>
2025-09-30 08:38:42 -0500

    Quote module: add models

---
+++ BellwoodGlobal.Mobile/BellwoodGlobal.Mobile/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace BellwoodGlobal.Mobile.Models;
+
+public class Location
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString("N");
+    public string Label { get; set; } = "";
+    public string Address { get; set; } = "";
+
+    public override string ToString()
+        => string.IsNullOrWhiteSpace(Label) ? Address : $"{Label} - {Address}";
+}
